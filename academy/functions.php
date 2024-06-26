7af0d686e4295331fa3bda80338a42c2884d13ad
CodingWithAbdou <coder.abdou@gmail.com>
2024-06-26 14:33:26 +0100

    - make call font better

--- academy/functions.php
+++ academy/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * wpAcademy functions and definitions
  *
@@ -7,9 +8,9 @@
  * @package wpAcademy
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +20,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function academy_setup() {
+function academy_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on wpAcademy, use a find and replace
 		* to change 'academy' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'academy', get_template_directory() . '/languages' );
+	load_theme_textdomain('academy', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +39,19 @@ function academy_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'academy' ),
+			'menu-1' => esc_html__('Primary', 'academy'),
 		)
 	);
 
@@ -83,7 +85,7 @@ function academy_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -100,7 +102,7 @@ function academy_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'academy_setup' );
+add_action('after_setup_theme', 'academy_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +111,24 @@ add_action( 'after_setup_theme', 'academy_setup' );
  *
  * @global int $content_width
  */
-function academy_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'academy_content_width', 640 );
+function academy_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('academy_content_width', 640);
 }
-add_action( 'after_setup_theme', 'academy_content_width', 0 );
+add_action('after_setup_theme', 'academy_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function academy_widgets_init() {
+function academy_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'academy' ),
+			'name'          => esc_html__('Sidebar', 'academy'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'academy' ),
+			'description'   => esc_html__('Add widgets here.', 'academy'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +136,36 @@ function academy_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'academy_widgets_init' );
+add_action('widgets_init', 'academy_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function academy_scripts() {
-	wp_enqueue_style( 'academy-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'academy-style', 'rtl', 'replace' );
+function academy_scripts()
+{
+	wp_enqueue_style('academy-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+	wp_enqueue_style('academy-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('academy-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'academy-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('academy-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'academy_scripts' );
+add_action('wp_enqueue_scripts', 'academy_scripts');
+
+function academy_resource_hints($url, $relation_type)
+{
+	if (wp_style_is('academy-fonts', 'queue') &&  'preconnect' == $relation_type) {
+		$url[] = array(
+			'href' => 'https://fonts.googleapis.com',
+			'crossorigin'
+		);
+	}
+	return $url;
+}
+add_filter('wp_resource_hints', 'academy_resource_hints', 1, 2);
 
 /**
  * Implement the Custom Header feature.
@@ -172,13 +190,13 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
 /**
  * Load WooCommerce compatibility file.
  */
-if ( class_exists( 'WooCommerce' ) ) {
+if (class_exists('WooCommerce')) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
